628bef3a8c2cce748646318d63f75cb46151d154
Zain Javed <42720762+Engr-zainjaved@users.noreply.github.com>
2025-05-01 13:02:59 +0500

    Update auth_controller.py

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -3,12 +3,18 @@ from odoo.http import request
 from werkzeug.utils import redirect
 
 class AutoLogin(http.Controller):
+    
     @http.route('/auth/bypass', type='http', auth='none', csrf=False)
     def auto_login(self, **kwargs):
         session_id = request.httprequest.args.get('session_id')
         if session_id:
-            response = redirect('/web')
+            # First set the cookie and redirect to a confirmation route
+            response = redirect(f'/auth/confirm')
             response.set_cookie('session_id', session_id, path='/', httponly=True)
             return response
-
         return redirect('/web/login')
+
+    @http.route('/auth/confirm', type='http', auth='none', csrf=False)
+    def confirm(self, **kwargs):
+        # At this point the browser has the session cookie
+        return redirect('/web')
