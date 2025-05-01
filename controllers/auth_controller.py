3b51fdb5e4bb7f36d61c6337d05190632e8fcc91
Zain Javed <42720762+Engr-zainjaved@users.noreply.github.com>
2025-05-01 12:02:53 +0500

    Update auth_controller.py

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,15 +1,30 @@
 from odoo import http
-from odoo.http import request
+from odoo.http import request, Response
 from werkzeug.utils import redirect
+import time
 
 class AutoLogin(http.Controller):
     @http.route('/auth/bypass', type='http', auth='none', csrf=False)
     def auto_login(self, **kwargs):
         session_id = request.httprequest.args.get('session_id')
-        if session_id:
-            response = redirect('/web')
-            response.set_cookie('session_id', session_id, path='/', httponly=True)
-            sleep(0.1)
-            return response
-
-        return redirect('/web/login')
+        if not session_id:
+            return redirect('/web/login')
+            
+        # Create a response that sets the cookie first
+        response = Response("""
+            <html>
+                <head>
+                    <meta http-equiv="refresh" content="0;url=/web" />
+                </head>
+                <body>
+                    <script>
+                        document.cookie = 'session_id={}; path=/; httponly'.replace('{}', '{value}');
+                    </script>
+                </body>
+            </html>
+        """.format('{value}', value=session_id))
+        
+        # Also set the cookie via HTTP headers as backup
+        response.set_cookie('session_id', session_id, path='/', httponly=True)
+        
+        return response
